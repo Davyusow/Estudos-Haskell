6c2c61ce2188d0558fc9895ea0176388e619aff8
Davyusow <dayvsonf27@gmail.com>
2025-09-18 00:05:55 -0300

    Adição dos arquivos do exercício 1 dos estudos de haskell

--- basico/exercicios01.hs
+++ basico/exercicios01.hs
@@ -1,3 +1,12 @@
 -- raio de círculo de raio r
 areaCirculo :: Float -> Float
 areaCirculo x = pi*(x*x) 
+
+perimetro :: Float -> Float
+perimetro x = 2*pi*x
+
+hipotenusa :: Float -> Float -> Float
+hipotenusa a b = sqrt(a*a+b*b)
+
+diffCirculo :: Float -> Float -> Float
+diffCirculo a b = abs(areaCirculo a - areaCirculo b)
